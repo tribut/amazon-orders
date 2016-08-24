b47aad88dfa8074e3e8f05da168d35afda6e9e3f
albalitz <albalitz@fb3.uni-bremen.de>
2016-08-24 02:26:30 +0200

    Replace requests with dryscrape to handle Amazon's JS.

--- amazon_orders.py
+++ amazon_orders.py
@@ -5,7 +5,7 @@ from getpass import getpass
 import os
 import json
 
-import requests
+import dryscrape
 from bs4 import BeautifulSoup
 
 
@@ -14,65 +14,45 @@ DEFAULT_LOGLEVEL = logging.WARNING
 logger = logging.getLogger(__name__)
 
 
-_AMAZON_DE_LOGIN_FORM_ACTION = "https://www.amazon.de/ap/signin"
 _AMAZON_DE_ORDER_HISTORY = "https://www.amazon.de/gp/your-account/order-history"
 
 
 _INCLUDE_FREE = False
 
 
-session = requests.Session()
+session = dryscrape.Session()
 session.headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
 
-def hidden_form_fields(form_descriptor, soup, by_attribute="name"):
-    form = [f for f in soup.find_all("form") if f.has_attr(by_attribute) and f[by_attribute] == form_descriptor][0]
-
-    if not form:
-        logger.critical("Form with {}: '{}' not found!".format(by_attribute, form_descriptor))
-        return None
-
-    data = {}
-    hidden_form_fields = [f for f in form.find_all("input") if f["type"] == "hidden"]
-    for hidden_field in hidden_form_fields:
-        name = hidden_field["name"]
-        value = hidden_field["value"]
-
-        data[name] = value
-
-    return data
-
-
 def login(email, password):
-    main_html = session.get("https://www.amazon.de")
-
     logger.info("Logging in...")
 
-    html = session.get(_AMAZON_DE_ORDER_HISTORY).content  # redirects to the signin page
+    session.visit(_AMAZON_DE_ORDER_HISTORY)  # redirects to the signin page
+    html = session.body()
     soup = BeautifulSoup(html, "html.parser")
 
-    login_data = hidden_form_fields("signIn", soup)
-    login_data["email"] = email
-    login_data["password"] = password
+    email_field = session.at_css("#ap_email")
+    password_field = session.at_css("#ap_password")
 
-    for k, v in login_data.items():
-        logger.debug("Login data: {} = {}".format(k, v))
+    email_field.set(email)
+    password_field.set(password)
 
-    r = session.post(_AMAZON_DE_LOGIN_FORM_ACTION, data=login_data)
-    h = r.content
+    session.at_css("#signInSubmit").click()  # email_field.form().submit() redirects to login with captcha. :(
+
+    h = session.body()
     s = BeautifulSoup(h, "html.parser")
 
     alerts = [a.string.strip() for a in s.select(".a-alert-container li") if a.string is not None]
     for alert in alerts:
         logger.error("from Amazon: {}".format(alert))
 
-    assert not alerts and "Ein Problem ist aufgetreten" not in s.string
+    assert not alerts and "Ein Problem ist aufgetreten" not in s
     logger.info("Login successful.")
 
 
-def extract_orders_from_page(soup):
+def extract_orders_from_page():
     """Extracts the orders from the page the selenium WebDriver is currently on.
 
     Loops through the elements found on the current order page and collects their data:
@@ -81,15 +61,12 @@ def extract_orders_from_page(soup):
     - the total amount
     - the link to amazon's details page
 
-    Arguments:
-        soup (BeautifulSoup) -- The BeautifulSoup instance for the website's html.
-
     Returns:
         [dict] -- a list containing the found orders
     """
     orders = []
 
-    order_elements = soup.select(".order")
+    order_elements = session.css(".order")
     for order_element in order_elements:
         order_number = None
         order_date = None
@@ -97,18 +74,18 @@ def extract_orders_from_page(soup):
         order_details_link = None
 
         # the whole top part of an order, with its date, sum, and order number
-        order_info = [order_element.select(".order-info")][0]
+        order_info = order_element.at_css(".order-info")
 
-        order_info_left = [order_info.select(".a-col-left")][0]
-        order_info_right = [order_info.select(".a-col-right")][0]
+        order_info_left = order_info.at_css(".a-col-left")
+        order_info_right = order_info.at_css(".a-col-right")
 
         # the left part's parts
-        info_cols = order_info_left.select(".a-column")
+        info_cols = order_info_left.css(".a-column")
         for col in info_cols:
             try:
-                description = [col.select(".a-size-mini")][0].string
-                value = [col.select(".a-size-base")][0].string
-            except IndexError:
+                description = col.at_css(".a-size-mini").text()
+                value = col.at_css(".a-size-base").text()
+            except AttributeError:
                 pass  # occurs for digital orders for the "recipient column"
 
             if description.lower() == "summe":
@@ -118,10 +95,8 @@ def extract_orders_from_page(soup):
                 order_date = value
 
 
-        order_number = [order_info_right.select(".a-size-mini")][0].string
-        order_details_link = [
-            order_info_right.select(".a-size-base a.a-link-normal").get_attribute("href")
-        ][0]
+        order_number = order_info_right.at_css(".a-size-mini").text()
+        order_details_link = order_info_right.at_css(".a-size-base a.a-link-normal").get_attr("href")
 
         order = {
             "order_number": order_number,
@@ -132,10 +107,10 @@ def extract_orders_from_page(soup):
         logger.info("Found order: {}".format(order))
 
         try:
-            if "Erstattet" in [order_element.select(".shipment")][0].string:
+            if "Erstattet" in order_element.at_css(".shipment").text():
                 logger.warning("Order {} was returned and refunded. Ignoring.".format(order_number))
                 continue
-        except IndexError:
+        except AttributeError:
             pass  # digital orders
 
         if not _INCLUDE_FREE and order_total == 0.0:
@@ -167,26 +142,25 @@ def download_orders(email, password, include_free=False):
         logger.critical("Login failed!")
         return None
 
-    orders_main_html = session.get(_AMAZON_DE_ORDER_HISTORY).content
-    orders_main_soup = BeautifulSoup(orders_main_html, "html.parser")
+    session.visit(_AMAZON_DE_ORDER_HISTORY)
 
-    year_selection_form_data = hidden_form_fields("timePeriodForm", orders_main_soup, by_attribute="id")
-    years = [o["value"] for o in orders_main_soup.select("select#orderFilter option") if "year" in o["value"]]
+    years = [o.get_attr("value") for o in session.css("#orderFilter option") if "year" in o.get_attr("value")]
 
     orders = []
     for year in years:
-        logger.info("Extracting {}...".format(year))
+        logger.info("Extracting {}...".format(year.replace("-", " ")))
+        session.at_css("#orderFilter").set(year)
+        session.at_css("#timePeriodForm").submit()
 
-        year_selection_form_data["orderFilter"] = year
-        orders_page_html = session.get(_AMAZON_DE_ORDER_HISTORY, data=year_selection_form_data)
+        orders_page_html = session.body()
         orders_page_soup = BeautifulSoup(orders_page_html, "html.parser")
 
-        ignore_classes = ["a-disabled", "a-selected", "a-last"]
-        pagination_urls = [li for li in orders_page_html.select("ul.a-pagination li") if li["class"] not in ignore_classes]
+        pagination_urls = [li.at_css("a").get_attr("href") for li in session.css("ul.a-pagination li") if li.get_attr("class") == "a-normal"]
 
         orders.extend(extract_orders_from_page(orders_page_soup))
         for pagination_url in pagination_urls:
-            html = session.get(pagination_url)
+            session.visit("https://www.amazon.de{}".format(pagination_url))
+            html = session.body()
             soup = BeautifulSoup(html, "html.parser")
 
             orders.extend(extract_orders_from_page(soup))
