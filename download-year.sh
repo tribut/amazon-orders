f17559c23ae0f0c3871e894cf49fcfa8a9c41dc3
Felix Eckhofer <felix@eckhofer.com>
2017-06-03 16:22:36 +0200

    Use correct paths

--- download-year.sh
+++ download-year.sh
@@ -4,7 +4,7 @@ MYDIR="$(dirname "$0")"
 . "$MYDIR/venv/bin/activate"
 
 YEAR="${1:-$(date +%Y)}"
-FILENAME="amazon-$YEAR"
+FILENAME="$MYDIR/amazon-$YEAR"
 
 set -x
-python3 amazon_orders.py -v -j "$FILENAME.json" -c "$FILENAME.csv" --single_year "$YEAR"
+python3 "$MYDIR/amazon_orders.py" -v -j "$FILENAME.json" -c "$FILENAME.csv" --single_year "$YEAR"
